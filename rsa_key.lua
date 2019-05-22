03243d756a12d6d08363b1aef9c9ac1b9c68476e
Annwan <anwankong@gmail.com>
2019-05-22 21:32:00 +0200

    .

--- rsa_key.lua
+++ rsa_key.lua
@@ -1,1 +1,13 @@
+------------------------------------------------------------------------------
+-- rsa_key.lua
+-- key generaration for RSA encryption
+------------------------------------------------------------------------------
+
+function rsa_key.generate(size)
+    local p = bigint_prime.generate_prime()
+    local q = bigint_prime.generate_prime()
+    while q == p do
+        q = bigint_prime.generate_prime()
+    end
+    local n = p*q
 
