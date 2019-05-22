4917e216f3eb06521a7b7f6e30059bfd493698cf
Annwan <anwankong@gmail.com>
2019-05-22 20:43:26 +0200

    .

--- import.lua
+++ import.lua
@@ -4,3 +4,5 @@
 ------------------------------------------------------------------------------
 
 _G.bigint_prime = loadfile("bigint_prime.lua")
+_G.bigint = loadfile("bigint.lua")
+_G.bigint_extra = loadfile("bigint_extra.lua")
