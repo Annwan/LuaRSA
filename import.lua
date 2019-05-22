339bc4c05203ac74166a1dcda336b6f9bb15504b
Annwan <anwankong@gmail.com>
2019-05-22 20:56:22 +0200

    reordered loadfile

--- import.lua
+++ import.lua
@@ -3,6 +3,11 @@
 -- global imports
 ------------------------------------------------------------------------------
 
-_G.bigint_prime = loadfile("bigint_prime.lua")
+-- Third-party librairies
+
 _G.bigint = loadfile("bigint.lua")
 _G.bigint_extra = loadfile("bigint_extra.lua")
+
+-- Specific Libraires - BigInt
+
+_G.bigint_prime = loadfile("bigint_prime.lua")
