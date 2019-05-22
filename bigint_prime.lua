8bf4b71a97abc153b2835aa015cf18f35f3d1996
Axelandre42 <5228298+Axelandre42@users.noreply.github.com>
2019-05-22 20:48:08 +0200

    Prime tests.

---
+++ bigint_prime.lua
@@ -0,0 +1,18 @@
+local bigint = loadfile('bigint.lua');
+local prime = {}
+
+prime.generate = function (length)
+    
+end
+
+prime.is_primal = function (prime)
+    local approx = bigint.new(5000)
+    local b = 0
+    for i = 1,100 do
+        b = prime / approx
+        approx = (b + approx) / 2
+    end
+    return approx
+end
+
+print(prime.is_primal(bigint.new(40000)))
